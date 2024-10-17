9bcee454fc35999dd99f2f4fbad44f116c1550e8
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-18 00:07:49 +0300

    Add files via upload

---
+++ raulgab/raulgab/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("Введите число:");
+    int A = int.Parse(Console.ReadLine());
+    if (A % 2 == 0 || A % 3 == 0)
+    {
+        Console.WriteLine("Число подходит");
+    }
+    else
+    {
+        Console.WriteLine("Число не подходит");
+    }
+}
+catch
+{
+    Console.WriteLine("Некорректные данные");
+} 
