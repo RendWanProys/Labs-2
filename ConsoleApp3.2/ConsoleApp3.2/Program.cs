fc308df000106272a2fbaa5351aedd341fc77e5d
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-10-31 00:42:07 +0300

    Add files via upload

---
+++ ConsoleApp3.2/ConsoleApp3.2/Program.cs
@@ -0,0 +1,39 @@
+﻿try
+{
+    {
+        Console.WriteLine("Вводите числа, отрицательное число завершает ввод:");
+        double number;
+        bool minus = false;
+        List<double> numbers = new List<double>();
+        do
+        {
+            if (!double.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Некорректный ввод. Введите число.");
+                continue;
+            }
+            if (number < 0)
+            {
+                minus = true;
+            }
+            else if (!minus)
+            {
+                numbers.Add(number);
+            }
+        } while (!minus);
+
+        Console.WriteLine("Числа в интервале от 3 до 13: ");
+        foreach (double num in numbers)
+        {
+            if (num >= 3 && num <= 13)
+            {
+                Console.Write(num + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+catch
+{
+    
+}
