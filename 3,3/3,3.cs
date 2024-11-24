7e524307741470e073b6bab07f5b3a4968981e32
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-11-25 00:06:43 +0200

    Add files via upload

---
+++ 3,3/3,3.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("Введите количество повторений");
+    int n= int.Parse(Console.ReadLine());
+    if (n <= 0)
+    {
+        Console.WriteLine("не должно быть меньше 0.");
+        return;
+    }
+    Console.WriteLine("Введите условия x");
+    double sum = 0;
+    int x = int.Parse(Console.ReadLine());
+    if (x == -1)
+        {
+        Console.WriteLine("Х не должен быть -1");
+        }
+
+    for (int j = 1; j <= 2 * n - 1; j += 2)
+    {
+        double res = 1 / j * Math.Pow((x - 1) / (x + 1), j);
+        sum += res;
+    }
+    Console.WriteLine(sum);
+}
+catch
+{
+    Console.WriteLine("Ошибка!");
+}
