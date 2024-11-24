3e7015015c2bc567d15cd207257a5aafdb7206fb
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-11-24 22:39:12 +0200

    Add files via upload

---
+++ 3,2 hard/3.2hard.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+	Console.Write("Введите n:");
+	int n = int.Parse(Console.ReadLine());
+	double s = 1;
+	for (int k = 1; k <= n; k++)
+		if (k != 3 && k != -7) s *= Math.Pow(k - 3, k - 5) * (k + 7)/(k!);
+	double res = s;
+	Console.WriteLine($"res={res:F2}");
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.Message);
+}
