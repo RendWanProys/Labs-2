e98b06acc930dd46233cc2afb3148cfcf02e93fd
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-12-07 18:54:15 +0300

    Add files via upload

---
+++ 5.2 middle/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.WriteLine("Введите текст, при наличи '«' или '»' они будут удалены!");
+    string input = Console.ReadLine();
+    string newText = input.Replace("«", "").Replace("»", "").Replace("<", "").Replace(">", "");
+    Console.WriteLine(newText);
+}
+catch
+{
+
+}
