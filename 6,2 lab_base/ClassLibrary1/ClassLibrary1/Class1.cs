6c4b1528726749b3889e1c07b914fa16f2a214f1
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-12-24 23:49:01 +0300

    Add files via upload

---
+++ 6,2 lab_base/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class MatrixOperations
+    {
+        public static void SwapDiagonals(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            if (n != matrix.GetLength(1)) throw new ArgumentException("Матрица должна быть квадратной.");
+
+            int[,] newMatrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) newMatrix[i, j] = matrix[i, n - 1 - i];
+                    else if (i == n - 1 - j) newMatrix[i, j] = matrix[i, i];
+                    else newMatrix[i, j] = matrix[i, j];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = newMatrix[i, j]; // Копирование элемента за элементом
+                }
+            }
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
