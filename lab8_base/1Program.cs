95119c7172f77696a9124266c0e2d93ae17ade33
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2024-12-26 22:23:37 +0200

    Add files via upload

---
+++ lab8_base/1Program.cs
@@ -0,0 +1,25 @@
+﻿public class 1Program
+{
+    static void Main(string[] args)
+    {
+        DateTime time;
+        Console.WriteLine("Введите дату и время (гггг-мм-дд чч:мм):");
+        string vrema = Console.ReadLine();
+        while (true)
+        {
+            if (DateTime.TryParse(vrema, out time))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+        }
+        DateTime startmonth = new DateTime(time.Year, time.Month, 1, 0, 0, 0);
+        TimeSpan timehours = time - startmonth;
+        double hours = timehours.TotalHours;
+        Console.WriteLine($"Введенная дата и время: {time}");
+        Console.WriteLine($"Прошло часов от начала месяца: {hours}");
+    }
+}
---
+++ lab8_base/2Program.cs
@@ -0,0 +1,27 @@
+﻿public class programm
+{
+    public static void Main(string[] args)
+    {
+        DateTime birth;
+        while (true)
+        {
+            Console.WriteLine("Введите дату рождения сотрудника (гггг-мм-дд):");
+            string input = Console.ReadLine();
+            if (DateTime.TryParse(input, out birth))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат даты. Попробуйте еще раз.");
+            }
+        }
+        Console.WriteLine("\nЮбилейные даты:");
+        for (int age = 5; age <= 100; age += 5)
+        {
+            DateTime jubel = birth.AddYears(age);
+            int dayOfWeek = (int)jubel.DayOfWeek == 0 ? 7 : (int)jubel.DayOfWeek;
+            Console.WriteLine($"{jubel:yyyy-MM-dd} - {jubel.Year - birth.Year} лет - День недели: {dayOfWeek}");
+        }
+    }
+}
