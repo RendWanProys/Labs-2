558878e91e923869bb683839a92e358cc10c426e
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2025-06-16 03:40:33 +0300

    Add files via upload

---
+++ Lab11_v11.37/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите массив целых чисел через пробел:");
+        string input = Console.ReadLine();
+        try
+        {
+            List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            Console.WriteLine("Четные элементы: " + string.Join(" ", evenNumbers));
+            var endsWithZero = numbers.Where(n => n % 10 == 0);
+            Console.WriteLine("Элементы, оканчивающиеся нулем: " + string.Join(" ", endsWithZero));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: Введены некорректные числа. Введите целые числа, разделенные пробелами.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+    }
+}
+
