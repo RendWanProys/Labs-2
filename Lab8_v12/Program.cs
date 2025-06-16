5760f7902e4816c1625f735a74619dadab2ba330
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2025-06-16 03:08:45 +0300

    Add files via upload

---
+++ Lab8_v12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<(double, double)> triangles = new Queue<(double, double)>();
+        Console.Write("Введите катет a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите катет b: ");
+        double b = double.Parse(Console.ReadLine());
+        triangles.Enqueue((a, b));
+        (double firstA, double firstB) = triangles.Peek();
+        double hypotenuse = Math.Sqrt(firstA * firstA + firstB * firstB);
+        double perimeter = firstA + firstB + hypotenuse;
+
+        Console.WriteLine("Периметр первого треугольника: " + perimeter);
+    }
+}
