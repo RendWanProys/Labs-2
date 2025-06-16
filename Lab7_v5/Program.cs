0c57a9fe7a15347301583a0f86260dd131568961
RendWanProys <122683762+RendWanProys@users.noreply.github.com>
2025-06-16 03:03:29 +0300

    Add files via upload

---
+++ Lab7_v5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> array = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            array.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int min = array.Min();
+
+        Console.WriteLine("Наименьший элемент: " + min);
+    }
+}
